14a751de51b06ef357d7ce58dc2004337016233f
Nourhan Essam <nourhan.essam.makhlouf@gmail.com>
2025-04-08 17:47:31 +0200

    Refactored project structure and added NUnit test project for service-level testing

--- Appointment_Management_System_Backend/src/Appointment_System.Application/Services/Implementaions/DoctorQualificationService.cs
+++ Appointment_Management_System_Backend/src/Appointment_System.Application/Services/Implementaions/DoctorQualificationService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Appointment_System.Application.DTOs.DoctorQualification;
+﻿using Appointment_System.Application.DTOs.DoctorQualification;
 using Appointment_System.Application.Services.Interfaces;
 using Appointment_System.Infrastructure.Repositories.Interfaces;
 
